959379b630d656918a54c6e6f9ec82590954fc18
Apprentice <apprentice@dbc-chicago031.local>
2013-11-25 15:51:06 -0600

    Added instance variables for use in view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,26 @@
 class UsersController < ApplicationController
   def index
+    @page_num = 1
+    @is_first_page = true
+    @last_page_num = (User.count / 50.0).ceil
+    @is_last_page = (@page_num == @last_page_num)
+
     @users = User.by_karma.limit(50)
   end
 
   def page_num
+    if params[:page_num].nil? || (params[:page_num].to_i == 1)
+      @page_num = 1
+      @is_first_page = true
+    else
+      @page_num = params[:page_num].to_i
+      @is_first_page = false
+    end
+
+    @last_page_num = (User.count / 50.0).ceil
+
+    @is_last_page = (@page_num == @last_page_num)
+
     @users = User.page(params[:page_num].to_i)
     render :index
   end
