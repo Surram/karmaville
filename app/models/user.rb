a7ab40159d6aa749fac263135baba4e3573247e0
Apprentice <apprentice@dbc-chicago031.local>
2013-11-25 11:30:05 -0600

    changed total_karma and by_karma methods and added self.page method

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,14 +18,23 @@ class User < ActiveRecord::Base
             :uniqueness => {:case_sensitive => false}
 
   def self.by_karma
-    joins(:karma_points).group('users.id').order('SUM(karma_points.value) DESC')
+    # joins(:karma_points).group('users.id').order('SUM(karma_points.value) DESC')
+    order('karma_points_total DESC')
   end
 
   def total_karma
-    self.karma_points.sum(:value)
+    # self.karma_points.sum(:value)
+    karma_points_total
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.page(page_number = nil)
+    page_number = 1 if page_number.nil?
+    num = ((page_number - 1) * 50) + 1
+    User.by_karma.limit(50).offset(num)
+  end
+
 end
