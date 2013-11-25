1bcaee1ed2271c47dcc349df5a0dca6be694e66e
Apprentice <apprentice@dbc-chicago031.local>
2013-11-25 11:28:56 -0600

    Reverted changes to seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,18 +15,18 @@ TOTAL_USERS = 100_000
 SLICE_SIZE  = 20_000
 
 fields = [:first_name, :last_name, :email, :username, :created_at, :updated_at]
+
 TOTAL_USERS.times.each_slice(SLICE_SIZE).each_with_index do |ids, index|
   data = ids.map do |i|
     [Faker::Name.first_name, Faker::Name.last_name, "email_#{i}@example.com", "user_#{i}", now, now]
   end
 
   puts "Inserted #{(index + 1)*SLICE_SIZE} of #{TOTAL_USERS} users..."
-  User.import(fields, data, :validate => false, :timestamps => false)  
+  User.import(fields, data, :validate => false, :timestamps => false)
 end
 
 user_ids = User.pluck(:id)
 fields = [:user_id, :value, :label, :created_at, :updated_at]
-
 
 TOTAL_KARMA.times.each_slice(SLICE_SIZE).each_with_index do |ids, index|
   data = ids.map do
