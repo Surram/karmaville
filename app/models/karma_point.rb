35467d5cb6d55b83744d9d4995605b89d68d27dc
Apprentice <apprentice@dbc-chicago031.local>
2013-11-25 11:30:31 -0600

    Added before save update to karma_point to update users KP value

--- app/models/karma_point.rb
+++ app/models/karma_point.rb
@@ -5,4 +5,15 @@ class KarmaPoint < ActiveRecord::Base
   validates :user, :presence => true
   validates :value, :numericality => {:only_integer => true, :greater_than_or_equal_to => 0}
   validates :label, :presence => true
+
+  before_save :update_users_karma_points
+
+  def update_users_karma_points
+    user = User.find(user_id)
+
+    total_karma_points = user.karma_points_total + value
+
+    user.update_attributes(:karma_points_total => total_karma_points)
+    user.save!
+  end
 end
