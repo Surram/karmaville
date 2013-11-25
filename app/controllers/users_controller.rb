991762908f7f616149e2def9d7cf6a219b7edd8a
Apprentice <apprentice@dbc-chicago031.local>
2013-11-25 11:31:04 -0600

    Added page_num method to display appropriate page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def index
     @users = User.by_karma.limit(50)
   end
+
+  def page_num
+    @users = User.page(params[:page_num].to_i)
+    render :index
+  end
 end
